dafbb832012f504038864130f5c4b1080e5325b4
adva-mo <advamozes@gmail.com>
2022-12-24 11:10:02 +0200

    Fixed comments controllers

--- server/controllers/comments.controllers.js
+++ server/controllers/comments.controllers.js
@@ -1,12 +1,15 @@
 const Snippet = require("../models/snippet.js");
 
 const addComment = async (req, res) => {
+  const { author, title, description, answerId } = req.body;
   try {
     const snippet = await Snippet.findById(req.params.snippetid);
     if (!snippet) return res.status(404).send("snippet is not exist");
-    snippet.comments.push(req.body);
+    const answer = snippet.answers.id(answerId);
+    if (!answer) return res.status(404).send("answer is not exist");
+    answer.comments.push({ author, title, description });
     await snippet.save();
-    res.send(snippet.comments[snippet.comments.length - 1]);
+    res.send(answer.comments[answer.comments.length - 1]);
   } catch (e) {
     res.status(404).send(e.message);
   }
@@ -16,10 +19,12 @@ const deleteComment = async (req, res) => {
   try {
     const snippet = await Snippet.findById(req.params.snippetid);
     if (!snippet) return res.status(404).send("snippet is not exist");
+    const answer = snippet.answers.id(req.body.answerId);
+    if (!answer) return res.status(404).send("answer is not exist");
 
-    const commentToRemove = snippet.comments.id(req.body.id);
+    const commentToRemove = answer.comments.id(req.body.id);
     if (commentToRemove) {
-      snippet.comments.pull(req.body.id);
+      answer.comments.pull(req.body.id);
       await snippet.save();
       res.send("comment removed");
     } else throw Error("comment isn't exist");
