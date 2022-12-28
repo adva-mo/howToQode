e4e8285eec5d271bd56aa102ec03f422c7bb2d5e
adva-mo <advamozes@gmail.com>
2022-12-28 09:31:03 +0200

    Added quesions-solved attrubute for User

--- server/models/User.js
+++ server/models/User.js
@@ -57,6 +57,10 @@ const userSchema = new mongoose.Schema(
       trim: true,
       default: "",
     },
+    solvedQuestions: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
